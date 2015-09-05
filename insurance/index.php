a35bfe96f7e876fd37aa3a24aa43e446b6b30cd2
kutyel <flaviocorpa@gmail.com>
2015-09-05 23:18:05 +0200

    Version 1.0

--- insurance/index.php
+++ insurance/index.php
@@ -4,13 +4,16 @@
   require '_assets/_libs/mustache/src/Mustache/Autoloader.php';
   Mustache_Autoloader::register();
 
+  // Get view to load
+  $v = $_GET['view'] ?: 'index';
+
   // Init template engine
   $m = new Mustache_Engine(array(
-      'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/_assets/_views'),
+      'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/_assets/_views', array('extension' => '.html')),
       'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/_assets/_partials')
   ));
 
-  // Render main html
-  print $m->render('index');
+  // Render view
+  print $m->render($v);
 
 ?>
